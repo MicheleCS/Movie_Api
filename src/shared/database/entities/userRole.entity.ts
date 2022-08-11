b39e9adeeccacc876a4268b35c522b03f98ad930
MicheleCS <michelesouza-@outlook.com>
2022-08-11 10:21:28 -0300

    Criação UserRole Entity e Repository

---
+++ src/shared/database/entities/userRole.entity.ts
@@ -0,0 +1,38 @@
+import {
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from 'shared/database/entities/user.entity';
+import { Role } from './role.entity';
+
+@Entity('user_roles')
+export class UserRole {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  public createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  public updatedAt: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  public deletedAt: Date;
+
+  @ManyToOne(() => User, (user) => user.userRoles, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'user_id' })
+  public user: User;
+
+  @ManyToOne(() => Role, (role) => role.userRoles, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'role_id' })
+  public role?: Role;
+}
